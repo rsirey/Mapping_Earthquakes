c958d7fad5a732249903cfeb59a1381f02b77663
rsirey <rsirey@gmail.com>
2021-11-14 10:10:34 -0500

    changes

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -45,8 +45,30 @@ let myStyle = {
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
+        // This function returns the style data for each of the earthquakes we plot on
+        // the map. We pass the magnitude of the earthquake into a function
+        // to calculate the radius.
+        function styleInfo(feature) {
+          return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: "#ffae42",
+            color: "#000000",
+            radius: getRadius(),
+            stroke: true,
+            weight: 0.5
+          };
+        }
+        // This function determines the radius of the earthquake marker based on its magnitude.
+	      // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+	      function getRadius(magnitude) {
+		            if (magnitude === 0) {
+			                  return 1;
+		            }
+		            return magnitude * 4;
+	      }
+        // Creating a GeoJSON layer with the retrieved data.
+        L.geoJson(data, {
 
     // We turn each feature into a circleMarker on the map.
     
@@ -54,5 +76,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 console.log(data);
                 return L.circleMarker(latlng);
             },
+          // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo
         }).addTo(map);
-    });
+});
